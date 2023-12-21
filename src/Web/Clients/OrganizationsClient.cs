1b62737ff4ff8c321d9ef44a9720d2f5516c8277
artem-sagadeev <sagadeev.artem2001@gmail.com>
2023-12-21 23:00:37 +0300

    add organizations client

---
+++ src/Web/Clients/OrganizationsClient.cs
@@ -0,0 +1,13 @@
+﻿namespace Web.Clients
+{
+    public class OrganizationsClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public OrganizationsClient(IConfiguration configuration)
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri(configuration["OrganizationsBaseAddress"]!);
+        }
+    }
+}
