06fe398d5697d6f35e6a8875dbf7583eb9aa9be2
artem-sagadeev <sagadeev.artem2001@gmail.com>
2023-12-21 22:37:36 +0300

    add event page backend

--- src/Web/Pages/Events/Event.cshtml.cs
+++ src/Web/Pages/Events/Event.cshtml.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Web.Clients;
+using Web.Dto;
 
 namespace Web.Pages.Events
 {
     public class Event : PageModel
     {
-        public void OnGet()
+        private readonly EventsClient _eventsClient;
+
+        public Event(EventsClient eventsClient)
         {
-            
+            _eventsClient = eventsClient;
+        }
+
+        public EventDto? EventDto;
+        
+        public async Task OnGet(Guid id)
+        {
+            EventDto = await _eventsClient.GetById(id);
         }
     }
 }
